e21d147c733a6e8eb97c78963ffd43d3d634b097
Oscar Villafuerte <mengo.villafuerte@gmail.com>
2016-01-05 07:57:58 -0600

    Clase 2 - Comentarios

---
+++ Clase 2 - Comentarios/holaMundo.rb
@@ -0,0 +1,16 @@
+=begin
+	Curso de Ruby
+	Codigo por Oscar Villafuerte
+	Tipos de comentarios
+=end
+class HolaMundo
+	def initialize()
+	end
+	def saluda()
+		puts "Hola Mundo" #Imprime el mensaje
+	end
+end
+# Inicializacion de la clase
+object = HolaMundo.new()
+object.saluda
+gets()
